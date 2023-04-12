129b7893f4bedf892c4b76d7f5c0ca8da26f5f0d
Hyeong <53553469+hyeongga@users.noreply.github.com>
2023-04-12 22:43:06 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,10 +1,10 @@
 N,K = map(int,input().split())
-
 coins = []
 for _ in range(N):
     coins.append(int(input()))
-coins.sort(reverse=True)
+coins.reverse()
 count = 0
+
 for coin in coins:
     count += K//coin
     K = K%coin
